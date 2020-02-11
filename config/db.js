5c0c0112037a354da12838daa824023941aa70ac
Leonardo Flores <contatoleonardoflores@gmail.com>
2020-02-10 22:32:50 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-  module.exports = {mongoURI: "mongodb+srv://dbLeonardoFlores:4c2b5f9n564@blogapp-z2z10.mongodb.net/test?retryWrites=true&w=majority"}
+  module.exports = {mongoURI: "mongodb+srv://dbLeonardoFlores:123456@blogapp-z2z10.mongodb.net/test?retryWrites=true&w=majority"}
 } else {
   module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
